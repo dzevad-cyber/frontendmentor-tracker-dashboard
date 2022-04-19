44fca8abe43441502c11eaab97fe7cfd101bb5a6
Dzevad <dzevad.del@gmail.com>
2022-04-19 15:43:17 +0200

    finished desktop design

--- components/time-tracking-dashboard/TimeTrackingDashboard.tsx
+++ components/time-tracking-dashboard/TimeTrackingDashboard.tsx
@@ -10,6 +10,7 @@ import { useTimeTrackerContext } from '../../context/timeTrackerContext/timeTrac
 const TimeTrackingDashboard: React.FC = () => {
   const { state } = useTimeTrackerContext();
 
+  const name = state.name.split(' ');
   return (
     <div className={styles.dashboard}>
       <div className={styles.header}>
@@ -17,13 +18,18 @@ const TimeTrackingDashboard: React.FC = () => {
           <div className={styles.img}>
             <Image
               src={'/assets/images/image-jeremy.png'}
-              width={65}
-              height={65}
+              // width={65}
+              // height={65}
               alt=''
+              layout='fill'
             />
           </div>
           <p className={styles.text}>
-            Report for <span className={styles.name}>{state.name}</span>
+            Report for{' '}
+            <span className={styles.name}>
+              <span>{name[0]}</span>
+              <span>{name[1]}</span>
+            </span>
           </p>
         </div>
         <Nav />
