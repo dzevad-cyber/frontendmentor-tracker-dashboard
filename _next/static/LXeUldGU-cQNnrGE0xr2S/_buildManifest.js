1e04b18518776d63dc26240feedcb3a06f4e677e
Dzevad <dzevad.del@gmail.com>
2022-04-19 18:49:48 +0200

    Updates

---
+++ _next/static/LXeUldGU-cQNnrGE0xr2S/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7ab055cce7317698.css","static/chunks/pages/index-b3a274fb764e80b6.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
